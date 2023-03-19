82c6e94e7b55fafce2eb177a2460d5140f15c0ca
sheep <82494123+thejanjan@users.noreply.github.com>
2023-03-19 15:02:05 -0700

    beam: registration action

--- registerAction.php
+++ registerAction.php
@@ -12,11 +12,37 @@ or die('Error connecting to MySQL server.');
   <title>Registration Action</title>
   </head>
   
-  <body bgcolor="white">
+	<body bgcolor="white">
   
   
   <hr>
+
+  <?php
   
+$username = $_POST['username'];
+
+if (strlen($username) > 20) {
+	print "ERROR: Username is too long. You scoundrel. Look at this foolish name: ".$username;
+} else if (strlen($username) <= 0) {
+	print "ERROR: Please specify a valid username!!";
+} else {
+	$username = mysqli_real_escape_string($conn, $username);
+	$query = "INSERT INTO user VALUES ('".$username."', null);";
+
+	$result = mysqli_query($conn, $query)
+	or die(mysqli_error($conn));
+
+	print "The username '".$username."' has been registered. Excellent!;";
+	print "Please head back to use your brand new account.";
+
+	mysqli_free_result($result);
+}
+
+// cleanup
+mysqli_close($conn);
+
+?>
+
 </body>
 </html>
 	  
