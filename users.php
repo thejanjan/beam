87dae4351b644fab206a028590ea0f376cb3e7a5
sheep <82494123+thejanjan@users.noreply.github.com>
2023-03-19 16:08:29 -0700

    userlist: format cleanup

--- users.php
+++ users.php
@@ -28,7 +28,8 @@ $row_count = mysqli_num_rows($result);
 
 print "The Beam service is empowered by <b>$row_count brilliant members!</b><br>(And counting!!)";
 print "<br><h4>All Members</h4>";
-print "<table style='width: 2px;'><tbody>";
+print "<table border='1' cellpadding = '5' cellspacing = '5'><tbody>";
+print "<th>User</th><th>Join Date</th><th>Avatar</th>";
 
 $index = 0;
 while ($row = mysqli_fetch_array($result, MYSQLI_BOTH)) {
@@ -36,7 +37,10 @@ while ($row = mysqli_fetch_array($result, MYSQLI_BOTH)) {
 	print "<tr>";
 	print "<td>User #$index:<br>$row[username]</td>";
 	print "<td>Joined the Beam Family<br>$row[timestamp]</td>";
-	print "<td><p><iframe style='width: 100px; height: 100px; overflow: hidden;' src='$row[avatar_url]' width='100' height='100' scrolling='no'>Iframes not supported</iframe></p></td>";
+	if ($row[avatar_url] == "")
+		print "<td>No Avatar</td>";
+	else
+		print "<td><p><iframe style='width: 100px; height: 100px; overflow: hidden;' src='$row[avatar_url]' width='100' height='100' scrolling='no'>Iframes not supported</iframe></p></td>";
 	print "</tr>";
 }
 
