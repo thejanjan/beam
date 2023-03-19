20623cd9cef1ad0d5ffca438729f32271df93c4b
sheep <82494123+thejanjan@users.noreply.github.com>
2023-03-19 14:44:28 -0700

    beam: register base

---
+++ user/registerAction.php
@@ -0,0 +1,22 @@
+<?php
+
+include('./connectionData.txt');
+
+$conn = mysqli_connect($server, $user, $pass, $dbname, $port)
+or die('Error connecting to MySQL server.');
+
+?>
+
+<html>
+<head>
+  <title>Registration Action</title>
+  </head>
+  
+  <body bgcolor="white">
+  
+  
+  <hr>
+  
+</body>
+</html>
+	  
