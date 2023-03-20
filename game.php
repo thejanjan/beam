7be69583411a3eab7bc66554a8909b76013a361b
sheep <82494123+thejanjan@users.noreply.github.com>
2023-03-19 20:30:03 -0700

    game: i should take a break

--- game.php
+++ game.php
@@ -18,23 +18,16 @@ or die('Error connecting to MySQL server.');
   <?php
 
 
-print "1";
 $game_id = $_GET['g'];
-print "<br>$game_id";
 $game_query = "SELECT game_id, name, description, releasedate, cost, publisher, developer, website, image FROM game WHERE game_id='$game_id';";
-print "<br>query made";
 $game_result = mysqli_query($conn, $game_query);
-print "<br>result made";
 
-/*
 $row_count = mysqli_num_rows($game_result);
 
-print "<br>row count $row_count";
-
 if ($row_count == 0) {
-	print "<br>FAIL!";
+	die();
 } else {
-	$row = mysqli_fetch_array($game_result, MYSQLI_BOTH)
+	$row = mysqli_fetch_array($game_result, MYSQLI_BOTH);
 
 	print "<h1>$row[name]</h1><hr>";
 }
@@ -42,7 +35,6 @@ if ($row_count == 0) {
 // cleanup
 mysqli_free_result($game_result);
 mysqli_close($conn);
-*/
 
 ?>
 
