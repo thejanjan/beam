71c2a786ade9300e21736043578d3f048260baa5
sheep <82494123+thejanjan@users.noreply.github.com>
2023-03-19 19:24:51 -0700

    messaging: render msgs in table

--- messaging.php
+++ messaging.php
@@ -129,6 +129,36 @@ else {
 			print '<input type="text" name="message">';
 			print '<input type="submit" value="Send">';
 			print '</form>';
+
+			$msg_query = "SELECT message_id, sender, receiver, message, timestamp FROM privatemessage WHERE (sender == '$user_a' AND receiver == '$user_b') OR (sender == '$user_b' AND receiver == '$user_a') ORDER BY timestamp DESC;";
+			$msg_result = mysqli_query($conn, $msg_query)
+			or die(mysqli_error($conn));
+
+			$row_count = mysqli_num_rows($msg_result);
+	
+			if ($row_count != 0) {
+				print "<table border='1' cellpadding = '5' cellspacing = '5'><tbody>";
+				print "<th>Avatar</th><th>User</th><th>Message</th><th>Timestamp</th>";
+
+				$index = 0;
+				while ($row = mysqli_fetch_array($friend_result, MYSQLI_BOTH)) {
+					$avatar = $user_a_query[avatar_url];
+					if ($row[sender] == receiver) {
+						$avatar = $user_b_query[avatar_url];
+					}
+					$index = $index + 1;
+					print "<tr>";
+					print "<td><p><img alt='Cool Avatar' width='100' height='100' src='$avatar'></p></td>";
+					print "<td>$other_user</td>";
+					print "<td>$row[message]</td>";
+					print "<td>$row[timestamp]</td>";
+					print "</tr>";
+				}
+
+				print "</tbody></table>";
+			}
+
+			mysqli_free_result($msg_result);
 		}
 
 		mysqli_free_result($user_b_result);
